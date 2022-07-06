d95d7447f085da4720a8a82ad76a79013b406783
Moskalenko Serhii <mynameserhii@gmail.com>
2022-07-06 14:57:23 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import debounce from 'lodash.debounce';
 import { refs } from './js/refs.js';
 import { createImage, createItem } from './js/elCreators.js';
 import gallery from './js/gallery.js';
+import mod from './partials/menu.hbs';
+import modJSON from './partials/name.JSON';
 
 const onSearchInput = e => {
   const inputEl = e.target;
@@ -49,3 +51,30 @@ const onFormSubmit = e => {
 refs.input.addEventListener('input', debounce(onSearchInput, 300));
 refs.list.addEventListener('click', onListItemCkick);
 refs.form.addEventListener('submit', onFormSubmit);
+
+const data = new Date(1994, 8, 10, 10, 30, 25, 35);
+
+// const makePromise = (text, delay) => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(text), delay);
+//   });
+// };
+
+// const promiseA = makePromise('promiseA value', 1000);
+// const promiseB = makePromise('promiseB value', 3000);
+
+// Promise.race([promiseA, promiseB])
+//   .then(value => console.log(value))
+//   .catch(error => console.log(error));
+
+const makeGreeting = guestName => {
+  if (guestName === '' || guestName === undefined) {
+    return Promise.reject('Guest name must not be empty');
+  }
+
+  return Promise.resolve(`Welcome ${guestName}`);
+};
+
+makeGreeting('Mn')
+  .then(greeting => console.log(greeting))
+  .catch(error => console.error(error));
